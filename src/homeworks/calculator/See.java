6799c0ba9bbb556775cb00ceab76987a4a27746e
Sergey Kazakevich <sergey@overdrive.sg>
2020-02-02 13:57:57 +0300

    ahahaha

--- src/homeworks/calculator/See.java
+++ src/homeworks/calculator/See.java
@@ -2,13 +2,11 @@ package homeworks.calculator;
 
 public class Learn {
     public static void main(String[] args) {
-        // bla bla bla
-        // bla bla bla
-        // bla bla bla
-        // bla bla bla
-        // bla bla bla
-        // bla bla bla
-        // bla bla bla
+        // 45654
+        // 45654
+        // 45654
+        // 45654
+        // bla bgit add la bla
         // bla bla bla
     }
 }
