b2777a6ecfc93e26eaae1dd393faeb3f034b1abc
nyatta <nyatta@github.com>
2020-02-02 14:01:20 +0300

    test

--- src/homeworks/calculator/See.java
+++ src/homeworks/calculator/See.java
@@ -4,9 +4,9 @@ public class Learn {
     public static void main(String[] args) {
         // bla bla bla
         // bla bla bla
-        // bla bla bla
-        // bla bla bla
-        // bla bla bla
+        // bla bla bla 1111111111111
+        // bla bla bla 222222222222222
+        // bla bla bla 3333333333333
         // bla bla bla
         // bla bla bla
         // bla bla bla
